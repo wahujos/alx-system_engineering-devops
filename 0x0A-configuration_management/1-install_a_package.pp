94d322ef8ab0aa7cbf733f1e12b79271d62b0ba3
wahujos <jwahu30@gmail.com>
2024-01-19 11:57:11 +0000

    1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,9 +1,6 @@
 # install a package flask using Puppet
 
 package { 'flask':
-  ensure  => 'installed',
-  require => Exec['pip3 install flask']
-}
-exec { 'pip3 install flask':
-  command => '/usr/bin/pip3 install flask==2.1.0'
+  ensure   => '2.1.0',
+  provider => 'pip3'
 }

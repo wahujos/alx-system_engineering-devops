a9df1756f500944833765b5981274dcfe0c83248
wahujos <jwahu30@gmail.com>
2024-01-19 11:42:09 +0000

    1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -5,5 +5,5 @@ package { 'flask':
   require => Exec['pip3 install flask']
 }
 exec { 'pip3 install flask':
-  command => '/usr/bin/pip3 install flask 2.1.0'
+  command => '/usr/bin/pip3 install flask==2.1.0'
 }

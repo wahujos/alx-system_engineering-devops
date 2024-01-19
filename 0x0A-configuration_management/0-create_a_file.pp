84eb4c9ee7b830c0ad2310748368ad25320f024e
wahujos <jwahu30@gmail.com>
2024-01-19 11:06:15 +0000

    0-create_a_file.pp

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,5 +1,8 @@
+# This is the first line comment
+
 file { '/tmp/school':
-  ensure  => 'present',
+  ensure  => file,
+  path    => '/tmp/school',
   mode    => '0744',
   owner   => 'www-data',
   group   => 'www-data',

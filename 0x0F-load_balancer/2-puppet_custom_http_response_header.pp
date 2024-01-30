2c8b556364d06a9bb566a2105d7b206b0fccc409
wahujos <jwahu30@gmail.com>
2024-01-30 17:34:42 +0300

    2-puppet_custom_http_response_header.pp

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -12,7 +12,7 @@ package { 'nginx':
 file_line { 'http_header':
   path    => '/etc/nginx/nginx.conf',
   line    => "http {\n\tadd_header X-Served-By \"${hostname}\";",
-  match   => 'http {'
+  match   => 'http {',
   require => Package ['nginx'],
 }
 #Restart nginx

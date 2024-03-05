bca92ea5f8b2a27f0f16a96bc560686b1d785df1
wahujos <jwahu30@gmail.com>
2024-03-05 16:44:35 +0300

    0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,17 +1,26 @@
 #!/usr/bin/python3
-"""function that queries the Reddit API and returns the number of subscribers"""
+"""
+function that queries the Reddit API and
+returns the number of subscribers
+"""
 
 import requests
+import sys
+
 
 def number_of_subscribers(subreddit):
     """Returns number of subscibers
     """
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {"User-Agent": "my_application123"}
-    resp = requests.get(url, headers);
+    resp = requests.get(url, headers)
     if resp.status_code == 200:
-        data = resp.json()
-        no_of_subscribers = data["data"]["subscribers"]
+        subreddit_data = resp.json()
+        no_of_subscribers = subreddit_data.get("data").get("subscribers")
         return no_of_subscribers
     else:
         return 0
+
+
+if __name__ == "__main__":
+    number_of_subscribers(sys.argv[1])

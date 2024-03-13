ee3a303f7eccac6cfb58313b5ec5ba62ff151abb
wahujos <jwahu30@gmail.com>
2024-03-13 22:54:46 +0300

    1-user_limit.pp

---
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -0,0 +1,11 @@
+# change the hard and soft limit of the holberton user
+# the said configurations can be found in /etc/security/limits.conf
+
+exec {
+command => 'sed -i "/^holberton hard/s/4/5000/" /etc/security/limits/conf'
+path    => '/usr/local/bin/:/bin/'
+}
+exec {
+command => 'sed -i "/^holberton soft/s/5/5000/" /etc/security/limits/conf'
+path    => '/usr/local/bin/:/bin/'
+}

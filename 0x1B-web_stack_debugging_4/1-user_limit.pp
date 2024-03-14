82a191c9988bc9fd80157eae7b5e9537f76a1877
wahujos <jwahu30@gmail.com>
2024-03-14 10:58:46 +0300

    1-user_limit.pp

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -2,10 +2,10 @@
 # the said configurations can be found in /etc/security/limits.conf
 
 exec {
-command => 'sed -i "/^holberton hard/s/5/5000/" /etc/security/limits/conf'
+command => 'sed -i "/^holberton hard/s/5/5000/" /etc/security/limits.conf'
 path    => '/usr/local/bin/:/bin/'
 }
 exec {
-command => 'sed -i "/^holberton soft/s/4/5000/" /etc/security/limits/conf'
+command => 'sed -i "/^holberton soft/s/4/5000/" /etc/security/limits.conf'
 path    => '/usr/local/bin/:/bin/'
 }
